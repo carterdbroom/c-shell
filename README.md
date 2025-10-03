f5c35b1039c27f18e3ac2c247aca7a1b9725a796
carter <carterdbroom@gmail.com>
2025-10-03 13:37:23 +0000

    Mostly done, just need to add concurrency.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# c-shell
+# C-Shell
+**
+This is a command line tool that allows the user to execute linux commands. 
+`history` will display the last 5 commands executed. The are numbered based on a running total of commands that have been executed. 
+`!!` executes the last valid command in the history executed. 
+If you enter any valid linux command it will be exectued via system calls. 
---
+++ shell.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_LINE 80 /* The maximum length command */
+#define MAX_TOKENS 40 /* The maximum number of tokens we could have in a command */
+char *history[5]; /* Array holding the previous commands */
+unsigned int num_commands_executed;
+void add_new_arg_to_history(char *arg);
+void split_string(char *input_string, char *string_array[]);
+
+int main(void) {
+    char arguments[MAX_LINE]; 
+    int should_run = 1; /* flag to determine when to exit the program */
+
+
+    while (should_run) {
+        printf("osh>");
+        fflush(stdout);
+
+        /* read input, break if fgets fails */
+        if (!fgets(arguments, MAX_LINE, stdin)) {
+            printf("Failed reading input");
+            break;
+        } 
+
+        arguments[strcspn(arguments, "\n")] = 0; /* gets rid of newline character */
+        /* We use this pointer so we can reassign what argument is passed in if "!!" is used */
+        char *arguments_pointer = arguments;
+        
+        /* add the latest command call to history */
+        //printf("%s\n", arguments_pointer);
+        latest_command:
+        /* End the program when the user types exit */
+        if (!strcmp("exit()", arguments_pointer)) {
+            should_run = 0;
+            continue;
+        } 
+        else if(!strcmp("history", arguments_pointer)) {
+            if (!history[0]) {
+                printf("No commands in history.\n");
+                continue;
+            }
+            for (int i = 0; i < 5; i++) {
+                if (history[i]) {
+                    printf("%d. %s\n", num_commands_executed - i, history[i]);
+                }
+            }
+        }
+        else if (!strcmp("!!", arguments_pointer)) {
+            if (history[0]) {
+                arguments_pointer = history[0]; /* our argument is now the last command executed */
+                goto latest_command;
+            } 
+            else {
+                printf("No commands in history.\n");
+                continue;
+            }
+        }
+        else {
+            pid_t pid = fork();
+
+            /* Child process */
+            if (pid == 0) {
+                
+                char *args_array[MAX_TOKENS];
+                /* This gives us an array of the command split by spaces */
+                split_string(arguments_pointer, args_array);
+                
+                /* Execute the command in */
+                execvp(args_array[0], args_array);
+                
+                /* This only executes if execvp fails*/
+                printf("Not a valid command\n");
+                exit(1);
+            } 
+            /* Parent process. */
+            else {
+                wait(NULL);
+                add_new_arg_to_history(arguments_pointer); 
+                num_commands_executed += 1;
+            }
+        }
+    }
+
+    /* Freeing any memory that is being held in the history array. */
+    for (int i = 0; i < 5; i++) {
+        if (history[i]) {
+            free(history[i]);
+        }
+    }
+
+    return 0;
+}
+
+void add_new_arg_to_history(char *arg) {
+    /* We don't want to have the history or !! command in the history */
+    if (!strcmp("history", arg) || !strcmp("!!", arg)) {
+        return;
+    }
+    /* Preventing memory leaks when we shift the array elements by one */
+    if (history[4]) {
+        free(history[4]);
+    }
+    /* Shifting each element in the history array to the right */
+    for (int i = 4; i > 0; i--) {
+        history[i] = history[i - 1];
+    } 
+    /* Assigning the first element in the history array to the passed in argument */
+    history[0] = strdup(arg);
+}
+
+/* This function takes in a input string and an array */
+/* It splits the input string on spaces and then adds each element to the array */
+/* Adds a null terminator on the end since the execvp call needs a null terminator in the array */
+void split_string(char *input_string, char *string_array[]) {
+    
+    if (input_string == NULL || string_array == NULL) {
+        return;
+    }
+
+    int count = 0;
+    char *token = strtok(input_string, " ");
+    while (token != NULL && count < MAX_TOKENS - 1) {
+        string_array[count] = token;
+        count++;
+
+        token = strtok(NULL, " ");
+    }
+
+    string_array[count] = NULL; /* adds the null terminator on the end */
+}
